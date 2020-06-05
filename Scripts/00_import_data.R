92c746a1d45370880ae27a9680e70aa26e1fe8ca
Aaron Chafetz <achafetz@usaid.gov>
2020-06-05 17:21:46 -0400

    add in org hierarhcy file

--- Scripts/00_import_data.R
+++ Scripts/00_import_data.R
@@ -12,6 +12,7 @@
 library(googledrive)
 library(tidyverse)
 library(glamr)
+library(Wavelength)
 
 
 # GLOBAL VARIABLES --------------------------------------------------------
@@ -22,6 +23,7 @@ library(glamr)
   
   pd8_file <- "HFR_2020.08_Tableau_20200604.csv"
 
+  drive_fldr_hierarchy <- "1enffr2NUZAz5eabUAGmfdXKoKWnLlWZ5"
   
 # SETUP FOLDERS -----------------------------------------------------------
   
@@ -32,8 +34,18 @@ library(glamr)
   drive_auth()
 
 
+
+# ID CURRENT ORG HIERARCHY FILE -------------------------------------------
+
+  org_file <- drive_ls(as_id(drive_fldr_hierarchy), 
+                        pattern = "HFR_FY20_GLOBAL_orghierarchy") %>% 
+    pull(name)
+  
 # IMPORT ------------------------------------------------------------------
 
   import_drivefile(drive_fldr, tx_file)
   import_drivefile(drive_fldr, pd8_file)
+  import_drivefile(drive_fldr_hierarchy, org_file)
 
+
+  
